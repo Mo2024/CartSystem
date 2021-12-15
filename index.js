7238b5f7141aa08aae649ab4fd9b3f0d973b95ed
Mo2024 <mohdosama2025@gmail.com>
2021-12-15 07:25:46 +0300

    first push

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, '/static')))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.listen(3000, () => {
+
+    console.log("Listening to port 3000")
+
+})
