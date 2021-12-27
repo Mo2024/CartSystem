fb97f48a5f3872eca1bd61103f96af7b54382cca
Mo2024 <mohdosama2025@gmail.com>
2021-12-28 02:44:28 +0300

    More changes + MySql added

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const mysql = require('mysql')
+
+// Create Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'my_db',
+    port: '3000'
+});
+
+// Connect
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('MySql Connected')
+});
+
 const app = express();
-const path = require("path")
+const path = require("path");
+
+//Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE my_db'
+    db.query(sql, (err, result) => {
+
+    });
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
