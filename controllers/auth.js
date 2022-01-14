b8ac52558dfcd66d1fc2487772ddf318a5068939
Mo2024 <mohdosama2025@gmail.com>
2022-01-14 13:46:40 +0300

    Improvements on signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,29 @@
+const { db } = require("..");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
 exports.register = (req, res) => {
-    console.log(req.body);
-    res.send("Form submitted")
+    // console.log(req.body);
+
+    const { name, username, email, password, cfmPassword } = req.body
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
+        if (error) { console.log(error) };
+        if (result.length > 0) {
+            return res.render('signup.ejs', {
+                message: "Email is already registered"
+            });
+        } else if (password !== cfmPassword) {
+            return res.render('signup.ejs', {
+                message: "Passwords do not match"
+
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword)
+
+    })
+
 }
