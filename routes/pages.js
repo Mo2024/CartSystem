557e00f7dd575229629d26142777b40f2d2910ea
Mo2024 <mohdosama2025@gmail.com>
2022-01-14 08:40:35 +0300

    Code organized more

--- routes/pages.js
+++ routes/pages.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const router = express.Router();
 const functions = require('./functions')
-
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     let dropdownResults = await functions.navBar();
+
     res.render('home.ejs', { dropdownResults });
 });
+
 router.get('/login', (req, res) => {
     res.render('login.ejs');
 });
+
 router.get('/signup', (req, res) => {
     res.render('signup.ejs');
 });
